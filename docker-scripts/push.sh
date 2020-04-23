cd31ed9bc2a34cf795c44f4214fcd1e8df3b3e1a
devvj <devvjj5@gmail.com>
2020-04-24 04:01:19 +0800

    docker scripts

---
+++ docker-scripts/push.sh
@@ -0,0 +1,2 @@
+docker push jiajidev/mm-app:mm.5.20
+docker push jiajidev/mm-web:mm.5.20
---
+++ docker-scripts/tag.sh
@@ -0,0 +1,2 @@
+docker tag mattermost-docker_app jiajidev/mm-app:mm.5.20
+docker tag mattermost-docker_web jiajidev/mm-web:mm.5.20
